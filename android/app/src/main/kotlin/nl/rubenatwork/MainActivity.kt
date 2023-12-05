7e3566cc5cc934d2ed425d177bf4ece466b535e2
Ruben van den Engel <info@rubenatwork.nl>
2023-12-05 14:08:42 +0100

    feat: initial commit

---
+++ android/app/src/main/kotlin/nl/rubenatwork/MainActivity.kt
@@ -0,0 +1,6 @@
+package nl.rubenatwork.beer_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:beer_app/app/app.dart';
+import 'package:beer_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
